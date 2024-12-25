9bbc816b60ba9f3880f2e2d885451b981f19d115
mariem monceur <monceurmariem@gmail.com>
2024-12-25 17:50:15 +0100

    while

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 const listeMots=["Cachalot","Pétunia","Serviette"];
+const listePhrases=["Pas de panique !","La vie, l’univers et le reste","Merci pour le poisson"]
 var score =0;
 
-let mot1=prompt("ecrit un mot 1 :")
-if(mot1 === listeMots[0])
+let type;
+while(type !== "mots" && type !== "phrases")
 {
-    score++;
+     type=prompt("donner le type")
 }
+    switch (type){
+        case  "mots":
+            for (let i = 0 ; i<listeMots.length; i++)
+                {
+                    let mot1=prompt("ecrit un mot  : "+ i);
+                    if (mot1 === listeMots[i])
+                    {score++;}
+                }
+            break;
+            
+        
+        case "phrases":
+            for (let i = 0 ; i<listePhrases.length; i++)
+                {
+                    let mot1=prompt("ecrit un mot  : "+ i);
+                    if (mot1 === listePhrases[i])
+                    {score++;}
+                }
+            break;
+            }
 
-let mot2=prompt("ecrit un mot 2 :")
-if(mot2 === listeMots[1])
-{
-    score++;
-}
-
-let mot3=prompt("ecrit un mot 3 :")
-if(mot3 === listeMots[2])
-{
-    score++;
-}
 console.log(score);
-
-
