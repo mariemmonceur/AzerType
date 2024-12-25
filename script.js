ee5e9016b1b79ebc4cb2657eff80fd54a5896d71
mariem monceur <monceurmariem@gmail.com>
2024-12-25 13:54:13 +0100

    comparer si deux éléments ont exactement la même valeur

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello world !");
+const motApplication="Bonjour";
+let motUtilisateur=prompt("entrer un mot");
+console.log(motUtilisateur)
+
+if(motUtilisateur === motApplication){
+    console.log("bravo vous avez correctement tapé le mot")
+
+}else{
+    console.log("echec le mot nest pas correct")
+}
