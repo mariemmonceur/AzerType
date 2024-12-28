41241c0f3b22b234087d8e64f45ccaa88f98804d
mariem monceur <monceurmariem@gmail.com>
2024-12-28 22:44:37 +0100

    html

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,12 @@ let balise =document.getElementById("zoneProposition")
 console.log(balise)
 let baliseQuery= document.querySelector("#zoneProposition span")
 console.log(baliseQuery)
+let b = document.querySelectorAll(".zoneChoix input")
+console.log(b)
+
+for (let i=0 ;i< b.length;i++)
+{
+    console.log(b[i])
+}
 
 //lancerJeu()
